ff8d259e6470af1b3b2cfe5c1b4e42922e920944
Mustafa Reşit Tavus <sergici40@gmail.com>
2013-11-12 19:29:54 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,28 +3,71 @@ def htmlTagChecker(fileName)
   htmlContent  = File.read(fileName)
  
   allTags = []
-  startTags = []
-  finishTags = []
+  tags = []
   first_index = 0
   last_index  = 0
+  durum = true
+  
+  i=0;
+  
+  while durum == true and i<=htmlContent.length
+    
+    char = htmlContent[i]
 
-  for i in 0..htmlContent.length
-     char = htmlContent[i]
-
+    /Karsilasilan karakter < ise bir tag baslangicidir ve 
+    < karakterinin sirasini bir degiskene atiyoruz/
      if char == "<"
           first_index = i
      end
+     /Eger karsilasilan karakter > ise bir tag kapatmadir
+     ve if icerisindeki islemlere giriyoruz/
      if char == ">"
-         last_index = i-first_index+1
+        /Kapatma taginin oldugu yerin sirasini degiskene atiyoruz /
+        last_index = i-first_index+1
+         
+         /Tum taglari allTags listesine ekiyoruz.
+         Program sonunda bulunan tum taglari yazdirmak icin kullanacagiz/
          allTags.push(htmlContent[first_index,last_index])
-
+        
+      
+      /Eger tag icerisinde \/ isareti varsa bir kapatma tagidir ve ona gore isleme devam ediyoruz/
       if(htmlContent[first_index+1] == "/")
-        finishTags.push(htmlContent[first_index,last_index])
+        /Eger tags listesinde baslama tagi bulunamaz ama yeni bir kapatma tagi ile karsilasilirsa 
+        "else" icerisine ama tags'da baslama tagi varsa if ifadesine giriyoruz/
+        if !tags.empty?
+           
+          /Baslangic taglarini string ifade haline getiriyoruz ve basindaki "<" ifadesini kaldiriyoruz/
+          sTag = tags.last()
+          sTag = sTag.to_s.split(//)
+          sTag.shift(1)
+          startTagStr = sTag.join('')
+        
+          /Bitirme taglarini string ifade haline getiriyoruz ve basindaki <\/ ifadesini kaldiriyoruz/
+          fTag = htmlContent[first_index,last_index]
+          fTag = fTag.to_s.split(//)
+          fTag.shift(2)
+          finishTagStr = fTag.join('')
+          
+          /Son eklenen baslama tagini karsilasilan bitirme tagi ile karsilastiriyoruz. 
+          Eger ayni ise listeye eklenen baslangic tagini listeden kaldiriyoruz
+          Eger esit degilse durumu false yapiyoruz ve donguden cikiyoruz/
+          if startTagStr == finishTagStr
+            tags.pop()
+          else
+            durum = false            
+          end
+          
+        /Tag listesinde baslangic tagi kalmadi ama bitis tagi ile karsilasildi ise 
+        durumu 0 yapiyoruz ve while'dan cikiyoruz/
+        else
+          durum = false
+        end
+      /Tag icerisinde \/ isareti yoksa baslama tagidir ve taglar listesinin sonuna ekliyoruz/
       else
-        startTags.push(htmlContent[first_index,last_index])
+        tags.push(htmlContent[first_index,last_index])
       end
-
-     end
+    end
+    i += 1
   end
   
   puts "Dosya icerisinde bulunan tum HTML taglari : "
@@ -32,60 +75,18 @@ def htmlTagChecker(fileName)
   
   puts ""
   puts "HTML tag esleme sonucu : "
-  tagCheckerStatus = tagControl(startTags,finishTags)
-  if tagCheckerStatus
+  
+  if durum
     puts "HTML taglari dogru eslesmis"
   else
     puts "HTML taglari hatalidir."
   end
-  
+
 end
 
 def writeTags(tags)
-  
   for i in 0..tags.length
     puts tags[i]    
-  end
-end
-
-def tagControl(startTags,finishTags)
-  
-  
-  if startTags.length != finishTags.length
-    return false
-  end
-  
-  
-  for i in 0..startTags.length-1
-    
-    sTag = startTags[i]
-    sTag = sTag.to_s.split(//)
-    sTag.shift
-    startTagStr = sTag.join('')
-    
-    if !finishTags.empty?
-        for j in 0..finishTags.length-1
-          fTag = finishTags[j]
-          fTag = fTag.to_s.split(//)
-          fTag.shift(2)
-          finishTagStr = fTag.join('')
-          
-          if startTagStr == finishTagStr
-            
-              finishTags.delete_at(j)
-              break;
-          end
-        end
-    else 
-       return false
-    end    
-  end
-  
-  
-  if finishTags.empty?
-    return true
-  else
-    return false
   end
 end
   
